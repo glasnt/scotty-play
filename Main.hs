05a43eb5018529211573b12485a00c98b99b392b
Katie McLaughlin <katie@glasnt.com>
2014-09-04 09:29:13 +1000

    params!

--- Main.hs
+++ Main.hs
@@ -1,6 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Web.Scotty
+import qualified Data.Text as T
+import Data.Monoid (mconcat)
 
 main = scotty 3000 $ do
   get "/" $ do
-    html "Hello World!"
+    text "Hello World!"
+  
+  get "/hi" $ do
+    text "hi!"
+
+  get "/beam/:word" $ do
+    beam <- param "word"
+    html $ mconcat ["Scotty, beam ", beam, " up!"]
+
+  notFound $ do
+    text "404: Page not Found. Sorry :<" 
